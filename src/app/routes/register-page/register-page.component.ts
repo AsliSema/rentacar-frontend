0bce2061de41a4d195ac8a3d4f7bec465c14b6b8
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-05-22 18:15:28 +0300

    create auths feature

--- src/app/routes/register-page/register-page.component.ts
+++ src/app/routes/register-page/register-page.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HomeLayoutComponent } from '../../shared/layouts/home-layout/home-layout.component';
-import { RegisterFormComponent } from '../../features/users/components/register-form/register-form.component';
+import { RegisterFormComponent } from '../../features/auths/register-form/register-form.component';
 
 @Component({
   selector: 'app-register-page',
