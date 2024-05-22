3259f9ae0e35e80071775457af51ded3fc6cf70a
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-05-22 17:53:03 +0300

    add register page

---
+++ src/app/routes/register-page/register-page.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HomeLayoutComponent } from '../../shared/layouts/home-layout/home-layout.component';
+import { RegisterFormComponent } from '../../features/users/components/register-form/register-form.component';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [CommonModule, HomeLayoutComponent, RegisterFormComponent],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss'
+})
+export class RegisterPageComponent {
+
+}
---
+++ src/app/routes/register-page/register.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { RegisterPageComponent } from "./register-page.component";
+
+export const registerRoutes: Routes = [
+    {
+        path: "register",
+        component: RegisterPageComponent
+    }
+
+]
