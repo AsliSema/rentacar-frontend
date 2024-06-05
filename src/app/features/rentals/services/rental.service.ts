c11c2bafce521e6b976d56c5dfed6bc5425ad129
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-06-05 20:44:49 +0300

    update rentals

--- src/app/features/rentals/services/rental.service.ts
+++ src/app/features/rentals/services/rental.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { GetRentalsByUserId } from '../models/get-rental-by-user-id.response.dto';
 import { Observable } from 'rxjs';
+import { RentalListItemDto } from '../models/rental-list-item.dto';
+import { RentalAddItemDto } from '../models/rental-add-item.dto';
+import { GetRentalByIdDto } from '../models/get-rental-by-id.dto';
+import { RentalUpdateRequestDto } from '../models/rental-update-request.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +16,25 @@ export class RentalService {
   private readonly controllerUrl = `${environment.apiUrl}/rentals`;
 
   constructor(private httpClient: HttpClient) { }
+
+
+  createRental(rental: RentalAddItemDto): Observable<void> {
+    return this.httpClient.post<void>(this.controllerUrl, rental);
+  }
+
+  getRentals(): Observable<RentalListItemDto[]>{
+    return this.httpClient.get<RentalListItemDto[]>(this.controllerUrl);
+  }
+
+  getRentalById(id: number): Observable<GetRentalByIdDto>{
+    const url = `${this.controllerUrl}/${id}`; 
+    return this.httpClient.get<GetRentalByIdDto>(url);
+  }
+
+  updateRentalById(id: number, request: RentalUpdateRequestDto): Observable<void>{
+    const url = `${this.controllerUrl}/${id}`; 
+    return this.httpClient.put<void>(url, request);
+  }
 
   getRentalsByUserId(id:number): Observable<GetRentalsByUserId[]>{
     const url = `${this.controllerUrl}/user/${id}`;
